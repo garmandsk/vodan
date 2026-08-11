a1d14332aa9a3384a03b844b0672274ab0ccaae9
Garmandsk <xmiagertiarmanda@gmail.com>
2026-08-11 21:18:51 +0700

    feat: added workspace_auth

--- app/lib/core/routes/app_router.dart
+++ app/lib/core/routes/app_router.dart
@@ -4,7 +4,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:vodan/core/providers/supabase_provider.dart';
 import 'package:vodan/core/routes/error_routes.dart';
 import 'package:vodan/features/pos/workspace_screen.dart';
